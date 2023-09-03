8ca5e37c413d5e9a9e75642a61deb2a80c0c89e7
vchandra22 <vincentchaandra@gmail.com>
2023-09-03 21:11:31 +0700

    day 2

--- app.js
+++ app.js
@@ -5,28 +5,51 @@ const app = Vue.createApp({
     //menampilkan data dinamis dari vue
     data() {
         return {
+            google: "https://www.google.co.id",
             showBooks: true,
             title: 'How to Became Good Programmer',
             author: 'Rendy Yulianto',
-            age: 43
+            age: 43,
+            x: 0,
+            y: 0,
+
+            // data buku dalam array
+            books: [
+                { judul: "Bad Habbits", author: "David J Alexander", img: "assets/images-1.jpeg", isFav: true },
+                { judul: "Little teacher", author: "Roman Wepkins", img: "assets/images-2.jpeg", isFav: false },
+                { judul: "Atomic Habbits", author: "Kyle Walker", img: "assets/images-3.jpeg", isFav: true }
+            ]
         }
     },
-    //membuat method untuk mengganti data->title
-    // methods: {
-    //     gantiJudul() {
-    //         this.title = 'How to Write Clean Code'
-    //     },
-    //     gantiTitle(title) {
-    //         this.title = title
-    //     }
-    // }
-
-    //Conditional rendering methods
     methods: {
+        //membuat method untuk mengganti data->title
+        gantiJudul() {
+            this.title = 'How to Write Clean Code'
+        },
+        gantiTitle(title) {
+            this.title = title
+        },
+
+        //Conditional rendering methods
         toogleShowBooks() {
             this.showBooks = !this.showBooks
+        },
+
+        // Mouse Events
+        eventHandler(e, data) {
+            console.log(e, e.type)
+            if (data) {
+                console.log(data)
+            }
+        },
+
+        // Mouse Move Event
+        handleMouseMove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
+
 })
 
 app.mount('#app')
